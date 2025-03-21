f2dd4fb7397e85bb472bfd062b31af1b5b7e4cdc
Louka BRARD <loukabrard5@gmail.com>
2025-03-21 14:06:30 +0100

    test ok +casesensitive

--- horspool.py
+++ horspool.py
@@ -13,3 +13,58 @@ def naive_search(text, pattern, case_sensitive=True) :
         if j == p :
             result.append(i)
     return result
+
+
+def shift(pattern, case_sensitive=True):
+    """
+    Retourne le dictionnaire des sauts pour les différentes lettres du motif (on exclu la dernière lettre)"""
+    D = {}
+    for i, letter in enumerate(pattern[:-1]) :
+        if case_sensitive :
+            D[letter] = len(pattern)-1-i
+        else :
+            letter = letter.upper()
+            D[letter] = len(pattern)-1-i
+    return D
+        
+assert shift('CGGCAG') == {'C':2, 'G':3, 'A':1}
+
+
+def search(text, pattern, case_sensitive=True):
+    """
+    Cherche un motif dans un texte
+    Retourne Vrai si le motif est trouvé, faux sinon
+    """
+    t = len(text)
+    p = len(pattern)
+    # Si le motif est plus long que le texte ou que le texte est vide, on retourne False
+    if t == 0 or p>t:# Si le motif est vide ou +grand que le texte
+        return []
+    # On crée la table des sauts
+    jumps = shift(pattern, case_sensitive=case_sensitive)
+    results = []
+    # On initialise l'indice i à 0
+    i = 0
+    while i<=t-p:
+        j = p-1
+        if case_sensitive :
+            while j >= 0 and text[i+j] == pattern[j] :
+                j -=1
+        else :
+            while j >= 0 and text[i+j].upper() == pattern[j].upper() :
+                j -=1
+        if j == -1 :
+            results.append(i)
+            i+=1
+        else :
+            if  case_sensitive :
+                jump = jumps.get(text[i+j], p) #saut sinon saut complet
+            else :
+                jump = jumps.get(text[i+j].upper(), p)
+            decalage = p-1-j
+            real_jump = jump-decalage
+            if real_jump < 0 :
+                i+=1
+            else :
+                i+= real_jump
+    return results
--- test_horspool.py
+++ test_horspool.py
@@ -1,4 +1,4 @@
-from horspool import naive_search
+from horspool import naive_search, shift, search
 
 #   +---------------------------------------------------+
 #   |                     TESTS                         |
@@ -18,4 +18,23 @@ DATAS_SEARCH = (
 #   ------NAIVE_SEARCH-------------
 for text, pattern, case_sensitive, expected in DATAS_SEARCH:
     result = naive_search(text, pattern, case_sensitive=case_sensitive)
-    assert expected==result, f"La recherche naïve de '{pattern}' dans '{text}' aurait du être {expected} mais c'est {result}."
+    assert expected==result, f"La recherche naïve de '{pattern}' dans '{text}', (case_sensitive = {case_sensitive}) aurait du être {expected} mais c'est {result}."
+
+#   ------SHIFT----------------------
+PATTERN = "aBcdCba"
+DATAS_SHIFT = (
+    # PATTERN         CASE_SENSITIVE  EXPECTED
+    (PATTERN,         True,           {'a': 6, 'B': 5, 'c': 4, 'd': 3, 'C': 2, 'b': 1}),
+    (PATTERN.lower(), True,           {'a': 6, 'b': 1, 'c': 2, 'd': 3}),
+    (PATTERN.title(), True,           {'A': 6, 'b': 1, 'c': 2, 'd': 3}),
+    (PATTERN,         False,          {'A': 6, 'B': 1, 'C': 2, 'D': 3}),
+)
+for pattern, case_sensitive, expected in DATAS_SHIFT: 
+    result = shift(pattern, case_sensitive=case_sensitive)
+    assert expected==result, f"Le motif '{pattern}' (case_sensitive={case_sensitive}) devrait donner {expected}"
+    
+    
+#   --------SEARCH-------------
+for text, pattern, case_sensitive, expected in DATAS_SEARCH:
+    result = search(text, pattern, case_sensitive=case_sensitive)
+    assert expected==result, f"La recherche de '{pattern}' dans '{text}', (case_sensitive = {case_sensitive}) aurait du être {expected} mais c'est {result}."
